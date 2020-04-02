774dae26e509101e5f08fb06d7ce5a0509b3b872
ahmetbcakici <ahmetbcakici@gmail.com>
2020-04-02 17:44:26 +0300

    data transfered between screens via react-navigation

--- screens/home.js
+++ screens/home.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, View, Text,Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Button, FlatList, TouchableOpacity} from 'react-native';
 
 import globalStyles from '../assets/styles/global';
 
 const Home = ({navigation}) => {
+  const [reviews, setReviews] = useState([
+    {
+      title: 'Zelda, Breath of Fresh Air',
+      rating: 5,
+      body: 'lorem ipsum',
+      key: '1',
+    },
+    {
+      title: 'Gotta Catch Them All (again)',
+      rating: 4,
+      body: 'lorem ipsum',
+      key: '2',
+    },
+    {title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3'},
+  ]);
 
-    const pressHandler = () => {
-            navigation.navigate('ReviewDetails')
-    }
+  const pressHandler = () => {
+    navigation.navigate('ReviewDetails');
+  };
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Button title="Go to Review Details Screen" onPress={pressHandler} />
+      <FlatList
+        data={reviews}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('ReviewDetails', item)}>
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
+import Navigator from '../routes/homeStack';
 import globalStyles from '../assets/styles/global';
 
-const ReviewDetails = () => {
+const ReviewDetails = ({route, navigation}) => {
+  const pressHandler = () => {
+    navigation.goBack();
+  };
+
+  const {key,title,body,rating} = route.params;
+
   return (
     <View style={styles.container}>
       <Text>ReviewDetails Screen</Text>
+      <Text>{title}</Text>
+      <Button title="Turn to back manually" onPress={pressHandler} />
     </View>
   );
 };
